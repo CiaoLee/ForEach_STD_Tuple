cba6b519d8d8068da21fef9e95f02a070f332129
ciaoli <ciaoli@git.code.tencent.com>
2019-05-08 16:39:00 +0800

    Add ApplyTupleToFuncImpl

--- MapOnTuple/TupleIterator.h
+++ MapOnTuple/TupleIterator.h
@@ -3,18 +3,33 @@
 #include <tuple>
 #include <type_traits>
 #include  <utility>
-//Transform tuple with recursion.
 
+
+template< std::size_t... Indices, typename FuncType,  typename...VarTypes, typename...ParamTypes>
+auto ApplyTupleToFuncImpl(std::tuple<VarTypes...>& InTuple, FuncType&& InFunc, ParamTypes&&... InParam)
+-> decltype(InFunc(std::forward<ParamTypes>(InParam)..., std::get<Indices>(InTuple)...))
+{
+	 return  InFunc(std::forward<ParamTypes>(InParam)..., std::get<Indices>(InTuple)...);
+}
+
+template<typename FuncType, typename... VarTypes, typename... ParamTypes>
+auto  ApplyTupleToFunc(std::tuple<VarTypes...>& InTuple, FuncType InFunc, ParamTypes... InParam)
+-> decltype(ApplyTupleToFuncImpl<std::index_sequence_for<VarTypes...>()>(InTuple, InFunc, InParam))
+{
+	return ApplyTupleToFuncImpl < std::index_sequence_for<VarTypes...>{} > (InTuple, InFunc, InParam ...);
+}
+
+//Transform tuple with recursion.
 template<std::size_t Index=0,typename FuncType,typename... VarTypes>
 inline typename std::enable_if<Index == sizeof...(VarTypes),void>::type
-TransformTuple(std::tuple<VarTypes...>& InTuple, FuncType InFunc )
+TransformTuple(std::tuple<VarTypes...>& InTuple, FuncType&& InFunc )
 {
 	//default;
 }
 
 template<std::size_t Index = 0,typename FuncType,typename... VarTypes>
 inline typename std::enable_if<Index < sizeof...(VarTypes),void>::type
-TransformTuple(std::tuple<VarTypes...>& InTuple, FuncType InFunc)
+TransformTuple(std::tuple<VarTypes...>& InTuple, FuncType&& InFunc)
 {
 	InFunc(std::get<Index>(InTuple));
 	TransformTuple<Index + 1>(InTuple, InFunc);
--- MapOnTuple/main.cpp
+++ MapOnTuple/main.cpp
@@ -8,6 +8,13 @@ void TuplePrintInt(int InVar)
 	std::cout << InVar << " ";
 }
 
+
+int TuplePrintInt4(int var1, int var2, int var3, int param1)
+{
+	std::cout << var1 << " " << var2 << " " << var3 << " " << param1 << " ";
+	return 4;
+}
+
 int main()
 {
 
@@ -15,8 +22,11 @@ int main()
 	std::function<void(int)> CallFunc= TuplePrintInt;
 
 	//Call TransformTuple
-	TransformTuple(IntsTuple,CallFunc);
+	TransformTuple(IntsTuple, CallFunc);
 	std::cout << std::endl;
+
+	std::function<int(int, int, int, int)> CallFunc2 = TuplePrintInt4;
+	std::cout<<ApplyTupleToFuncImpl<0,1,2>(IntsTuple, CallFunc2, 1)<<" ";
 
 	int Temp;
 	std::cin >> Temp;
